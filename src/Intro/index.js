9b5729fd41b40e62a499ac8f45feec035b7c7996
Suvel <suvelrathneswar08@gmail.com>
2022-06-04 19:37:27 +0530

    Adding intro

--- src/Intro/index.js
+++ src/Intro/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import './style.css'
+import wel_emoji from '../asset/wel_emoji.png'
 
 function Intro() {
   return (
-    <div className='Intro'>Intro</div>
+    <div className='Intro'>
+      <div className='welcomeBanner'>
+        <img src={wel_emoji} />
+        <p>Welcome to the ADMIN PAGE</p>
+      </div>
+    </div>
   )
 }
 
