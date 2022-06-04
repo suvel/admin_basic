6bffe5c856ed1f8e724b6265bc533323960bdd83
Suvel <suvelrathneswar08@gmail.com>
2022-06-04 12:51:53 +0530

    finished table

--- src/Table/index.js
+++ src/Table/index.js
@@ -1,9 +1,45 @@
 import React from 'react'
 import './style.css'
 
-function Table() {
+function Table({ data }) {
+
+  const getRow = (row) => {
+    if (row?.length > 0) {
+      return data.map(dObj => {
+        return (
+          <tr key={dObj?.id}>
+            <td><input type="checkbox" /></td>
+            <td>{dObj?.name}</td>
+            <td>{dObj?.email}</td>
+            <td>{dObj?.role}</td>
+          </tr>
+        )
+      })
+    }
+    else
+      return (<tr>
+        <td colSpan={4}>There no record to show</td>
+      </tr>)
+  }
+
   return (
-    <div className='Table'>Table</div>
+    <div className='Table'>
+      <table>
+        <thead>
+          <tr>
+            <th>{" "}</th>
+            <th>name</th>
+            <th>email</th>
+            <th>role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            getRow(data)
+          }
+        </tbody>
+      </table>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Workshop from './Workshop';
 // import reportWebVitals from './reportWebVitals';
+
+const testing = 1;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    !testing ? <App /> : <Workshop />
 );
 
 // If you want to start measuring performance in your app, pass a function
