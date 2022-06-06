89e08a1c9c783c7966b77cb0dc40a798f6227f42
Suvel <suvelrathneswar08@gmail.com>
2022-06-06 18:33:07 +0530

    Refactored Table selection login into a custom hook

--- src/Table/index.js
+++ src/Table/index.js
@@ -1,69 +1,10 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React from 'react'
 import './style.css'
+import useTableRowSelector from './useTableRowSelector.js'
 
 function Table({ data, onRowSelection = () => { }, selectedRow: selectedRows = [] }) {
 
-  const [tableData, setTableData] = useState(selectedRows);
-  const [isAllSelected, setIsAllSelected] = useState(false);
-
-  const toggleAllSelection = useCallback((currentData) => {
-    if (selectedRows.length == currentData.length && isAllSelected == false) {
-      setIsAllSelected(true)
-    }
-    else if (isAllSelected == true && (
-      currentData.length == 0 || selectedRows.length != currentData.length
-    )) {
-      setIsAllSelected(false)
-    }
-  }, [selectedRows])
-
-  const onSelectionChange = (event, id) => {
-    const isChecked = event.target.checked
-    //selected all 
-    if (isChecked == true && id == "all" && isAllSelected == false) {
-      console.log('selected all')
-      onRowSelection(data)
-      setIsAllSelected(true)
-    }
-    //unselected all
-    else if (isChecked == false && id == "all" && isAllSelected == true) {
-      console.log('unselected all')
-      onRowSelection([])
-      setIsAllSelected(false)
-    }
-    //select one
-    else if (isChecked == true && (id && id != "all")) {
-      console.log('selected ONE')
-      const selRow = data.find(item => item.id == id);
-      const newSelectedRow = [...selectedRows, selRow];
-      onRowSelection(newSelectedRow);
-      //when unselecting one, if all is selected, unselect all
-      toggleAllSelection(newSelectedRow)
-    }
-    //unselect one
-    else if (isChecked == false && (id && id != "all")) {
-      console.log('unselected one')
-      const newSelectedRow = selectedRows.filter(item => item.id != id)
-      onRowSelection(newSelectedRow);
-      //when unselecting one, if all is selected, unselect all
-      toggleAllSelection(newSelectedRow)
-    }
-  }
-
-  useEffect(() => {
-    const isRowSelected = (id) => {
-      return selectedRows.some(item => item.id == id)
-    }
-
-    const newTableData = data.map(item => {
-      const newItem = { ...item, isSelected: isRowSelected(item.id) }
-      return newItem;
-    })
-    setTableData(newTableData);
-
-    toggleAllSelection(data)
-
-  }, [selectedRows, data])
+  const { tableData, isAllSelected, onSelectionChange } = useTableRowSelector(data, selectedRows, onRowSelection)
 
   const getRow = (row) => {
     if (row?.length > 0) {
---
+++ src/Table/useTableRowSelector.js
@@ -0,0 +1,71 @@
+import { useState, useEffect, useCallback } from 'react'
+
+const useTableRowSelector = (data, selectedRows, updateSelectedRows) => {
+    const [tableData, setTableData] = useState(selectedRows);
+    const [isAllSelected, setIsAllSelected] = useState(false);
+
+    const toggleAllSelection = useCallback((currentData) => {
+        if (selectedRows.length == currentData.length && isAllSelected == false) {
+            setIsAllSelected(true)
+        }
+        else if (isAllSelected == true && (
+            currentData.length == 0 || selectedRows.length != currentData.length
+        )) {
+            setIsAllSelected(false)
+        }
+    }, [selectedRows])
+
+
+
+    useEffect(() => {
+        const isRowSelected = (id) => {
+            return selectedRows.some(item => item.id == id)
+        }
+
+        const newTableData = data.map(item => {
+            const newItem = { ...item, isSelected: isRowSelected(item.id) }
+            return newItem;
+        })
+        setTableData(newTableData);
+
+        toggleAllSelection(data)
+
+    }, [selectedRows, data])
+
+    const onSelectionChange = (event, id) => {
+        const isChecked = event.target.checked
+        //selected all 
+        if (isChecked == true && id == "all" && isAllSelected == false) {
+            console.log('selected all')
+            updateSelectedRows(data)
+            setIsAllSelected(true)
+        }
+        //unselected all
+        else if (isChecked == false && id == "all" && isAllSelected == true) {
+            console.log('unselected all')
+            updateSelectedRows([])
+            setIsAllSelected(false)
+        }
+        //select one
+        else if (isChecked == true && (id && id != "all")) {
+            console.log('selected ONE')
+            const selRow = data.find(item => item.id == id);
+            const newSelectedRow = [...selectedRows, selRow];
+            updateSelectedRows(newSelectedRow);
+            //when unselecting one, if all is selected, unselect all
+            toggleAllSelection(newSelectedRow)
+        }
+        //unselect one
+        else if (isChecked == false && (id && id != "all")) {
+            console.log('unselected one')
+            const newSelectedRow = selectedRows.filter(item => item.id != id)
+            updateSelectedRows(newSelectedRow);
+            //when unselecting one, if all is selected, unselect all
+            toggleAllSelection(newSelectedRow)
+        }
+    }
+
+    return { tableData, isAllSelected, onSelectionChange }
+}
+
+export default useTableRowSelector;
