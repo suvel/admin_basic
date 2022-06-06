9829139ec5dfe3eb1ea3a0077feaebf889e86f0e
Suvel <suvelrathneswar08@gmail.com>
2022-06-06 14:03:04 +0530

    Table selection functionality added

--- src/Table/index.js
+++ src/Table/index.js
@@ -1,14 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './style.css'
 
-function Table({ data }) {
+function Table({ data, onRowSelection = () => { }, selectedRow: selectedRows = [] }) {
+
+  const [tableData, setTableData] = useState(selectedRows);
+  const [isAllSelected, setIsAllSelected] = useState(false);
+
+
+  const onSelectionChange = (event, id) => {
+    const isChecked = event.target.checked
+    //selected all 
+    if (isChecked == true && id == "all") {
+      console.log('selected all')
+      onRowSelection(data)
+      setIsAllSelected(true)
+    }
+    //unselected all
+    if (isChecked == false && id == "all") {
+      console.log('unselected all')
+      onRowSelection([])
+      setIsAllSelected(false)
+    }
+    //select one
+    if (isChecked == true && (id && id != "all")) {
+      console.log('selected ONE')
+      const selRow = data.find(item => item.id == id);
+      const newSlectedRow = [...selectedRows,selRow];
+      onRowSelection(newSlectedRow);
+    }
+    //unselect one
+    if (isChecked == false && (id && id != "all")) {
+      console.log('unselected one')
+      const newSlectedRow = data.filter(item => item.id != id)
+      onRowSelection(newSlectedRow);
+      setIsAllSelected(false)
+    }
+  }
+
+  useEffect(() => {
+    const isRowSelected = (id) => {
+      return selectedRows.some(item => item.id == id)
+    }
+
+    const newTableData = data.map(item => {
+      const newItem = { ...item, isSelected: isRowSelected(item.id) }
+      return newItem;
+    })
+
+    setTableData(newTableData);
+  }, [selectedRows, data])
 
   const getRow = (row) => {
     if (row?.length > 0) {
-      return data.map(dObj => {
+      return tableData.map(dObj => {
         return (
           <tr key={dObj?.id}>
-            <td><input type="checkbox" /></td>
+            <td><input  checked={dObj.isSelected} type="checkbox" onChange={(e) => onSelectionChange(e, dObj.id)} /></td>
             <td>{dObj?.name}</td>
             <td>{dObj?.email}</td>
             <td>{dObj?.role}</td>
@@ -27,7 +74,7 @@ function Table({ data }) {
       <table>
         <thead>
           <tr>
-            <th>{" "}</th>
+            <th><input checked={isAllSelected}  type="checkbox" onChange={(e) => onSelectionChange(e, "all")} /></th>
             <th>name</th>
             <th>email</th>
             <th>role</th>
