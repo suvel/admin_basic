0553cf1d65016692efb3681ed0e5350f58fe29fe
Suvel <suvelrathneswar08@gmail.com>
2022-06-06 18:17:41 +0530

    Conditional rendering form and no selction banner

--- src/TableDataPallet/index.js
+++ src/TableDataPallet/index.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./style.css";
 import NoActionPlaceholder from "../NoActionPlaceholder";
 import Form from '../Form'
+import { TableContext } from '../context/tableContext'
 
 function TableDataPallet() {
+  const { selectedTableRow, updateSelectedTableRow } = useContext(TableContext);
+
+  const selectedTableCount = selectedTableRow?.length || 0;
+
+  const handelCallback = () => {
+    //clearing the selection
+    updateSelectedTableRow([]);
+  }
+
   return (
     <div className="TableDataPallet">
-      <div className="TableDataPallet_rowCount">Total Rows: 3</div>
-      {/* <NoActionPlaceholder /> */}
-      <Form />
+      <div className="TableDataPallet_rowCount">
+        Total Rows:{selectedTableCount}
+      </div>
+      {
+        selectedTableCount > 0 ? (
+          <Form onCancelClick={handelCallback} />
+        ) : (
+          <NoActionPlaceholder />
+        )
+      }
     </div>
   );
 }
