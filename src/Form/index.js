abbd403cb895f7c7b076ca929328cfa7ee28529b
Suvel <suvelrathneswar08@gmail.com>
2022-06-06 12:43:05 +0530

    Updated Form component

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import LabeledInput from '../LabeledInput'
+import Button from '../Button'
 import './style.css'
 
 const Form = () => {
@@ -8,6 +9,11 @@ const Form = () => {
             <LabeledInput labelValue={"Name"} type="text" placeholder="Name" />
             <LabeledInput labelValue={"Email"} type="text" placeholder="Email" />
             <LabeledInput labelValue={"Role"} type="text" placeholder="Role" />
+            <div className="Form_buttonsContainer">
+                <Button type='outline' variant='alert'>Delete</Button>
+                <Button type='outline' variant='carefree'>Edit</Button>
+                <Button>Cancel</Button>
+            </div>
         </div>
     )
 }
