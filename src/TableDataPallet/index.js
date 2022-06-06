184d1a635d12a2e55f04661aeb9e12746098b348
Suvel <suvelrathneswar08@gmail.com>
2022-06-06 12:18:01 +0530

    Placed Form inside TableDataPallet

--- src/TableDataPallet/index.js
+++ src/TableDataPallet/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./style.css";
 import NoActionPlaceholder from "../NoActionPlaceholder";
+import Form from '../Form'
 
 function TableDataPallet() {
   return (
     <div className="TableDataPallet">
       <div className="TableDataPallet_rowCount">Total Rows: 3</div>
-      <NoActionPlaceholder />
+      {/* <NoActionPlaceholder /> */}
+      <Form />
     </div>
   );
 }
