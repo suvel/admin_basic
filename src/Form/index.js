1813784255f1f65706276bd349b2ac0af392b179
Suvel <suvelrathneswar08@gmail.com>
2022-06-07 15:36:04 +0530

    Updated filed to be controllable

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LabeledInput from '../LabeledInput'
 import Button from '../Button'
 import './style.css'
 
-const Form = ({ onCancelClick }) => {
+const Form = ({ data, onCancelClick }) => {
+
+    const [fromData, setFormData] = useState(data);
+
+    const updateFormData = (event, filed) => {
+        setFormData(data => ({ ...data, [filed]: event.target.value }));
+    }
 
     return (
         <div className='Form'>
-            <LabeledInput labelValue={"Name"} type="text" placeholder="Name" />
-            <LabeledInput labelValue={"Email"} type="text" placeholder="Email" />
-            <LabeledInput labelValue={"Role"} type="text" placeholder="Role" />
+            <LabeledInput value={fromData?.name} onChange={(e) => updateFormData(e, "name")} labelValue={"Name"} type="text" placeholder="Name" />
+            <LabeledInput value={fromData?.email} onChange={(e) => updateFormData(e, "email")} labelValue={"Email"} type="text" placeholder="Email" />
+            <LabeledInput value={fromData?.role} onChange={(e) => updateFormData(e, "role")} labelValue={"Role"} type="text" placeholder="Role" />
             <div className="Form_buttonsContainer">
                 <Button type='outline' variant='alert'>Delete</Button>
                 <Button type='outline' variant='carefree'>Edit</Button>
