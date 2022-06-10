372aed7f73dc70179a17f39c3f4bb02df71c93d4
Suvel <suvelrathneswar08@gmail.com>
2022-06-10 14:04:30 +0530

    Integrating  table update function

--- src/Form/index.js
+++ src/Form/index.js
@@ -3,22 +3,26 @@ import LabeledInput from '../LabeledInput'
 import Button from '../Button'
 import './style.css'
 
-const Form = ({ data, onCancelClick, onDeleteClick }) => {
+const Form = ({ data, onCancelClick, onDeleteClick, onEdtClick }) => {
 
     const [fromData, setFormData] = useState(data);
 
-    const updateFormData = (event, filed) => {
+    const updateFormInput = (event, filed) => {
         setFormData(data => ({ ...data, [filed]: event.target.value }));
+    }
+
+    const updatData = () => {
+        onEdtClick(data.id, fromData);
     }
 
     return (
         <div className='Form'>
-            <LabeledInput value={fromData?.name} onChange={(e) => updateFormData(e, "name")} labelValue={"Name"} type="text" placeholder="Name" />
-            <LabeledInput value={fromData?.email} onChange={(e) => updateFormData(e, "email")} labelValue={"Email"} type="text" placeholder="Email" />
-            <LabeledInput value={fromData?.role} onChange={(e) => updateFormData(e, "role")} labelValue={"Role"} type="text" placeholder="Role" />
+            <LabeledInput value={fromData?.name} onChange={(e) => updateFormInput(e, "name")} labelValue={"Name"} type="text" placeholder="Name" />
+            <LabeledInput value={fromData?.email} onChange={(e) => updateFormInput(e, "email")} labelValue={"Email"} type="text" placeholder="Email" />
+            <LabeledInput value={fromData?.role} onChange={(e) => updateFormInput(e, "role")} labelValue={"Role"} type="text" placeholder="Role" />
             <div className="Form_buttonsContainer">
                 <Button onClick={() => onDeleteClick([data?.id])} type='outline' variant='alert'>Delete</Button>
-                <Button type='outline' variant='carefree'>Edit</Button>
+                <Button onClick={updatData} type='outline' variant='carefree'>Edit</Button>
                 <Button onClick={onCancelClick}>Cancel</Button>
             </div>
         </div>
