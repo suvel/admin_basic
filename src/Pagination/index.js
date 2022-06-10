02cdca3ac422680f7daa89fe6ac09fa37100095f
Suvel <suvelrathneswar08@gmail.com>
2022-06-10 08:50:14 +0530

    Added Pagination component

--- src/Pagination/index.js
+++ src/Pagination/index.js
@@ -1,20 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
 import arrow from '../asset/arrow.svg'
 
-const RoundButton = ({ children, ...props }) => {
-    return <button className='RoundButton' {...props} >{children}</button>
+const RoundButton = ({ children, selected, ...props }) => {
+    return <button className={`RoundButton ${selected ? "selected" : ""}`}  {...props} > {children}</button >
 }
 
-const Pagination = () => {
+const maxRowPerPage = 10;
+
+const Pagination = ({ numberOfRecords, onPageChange }) => {
+
+    const [curPage, setCurPage] = useState(1);
+
+    const PageCount = Math.ceil(numberOfRecords / maxRowPerPage);
+    const getPageNumber = () => {
+        let pageNumber = [];
+        for (let i = 1; i <= PageCount; i++) {
+            pageNumber.push(i);
+        }
+        return pageNumber;
+    }
+
+    const handelNavigation = (navigationId) => {
+        let newNavigationId = navigationId;
+        if (navigationId == "nxt") {
+            newNavigationId = curPage + 1
+        }
+        else if (navigationId == "prv") {
+            newNavigationId = curPage - 1
+        }
+        debugger;
+        setCurPage(newNavigationId);
+        onPageChange((newNavigationId - 1) * maxRowPerPage, maxRowPerPage * newNavigationId);
+    }
+
+    useEffect(() => {
+        onPageChange(0, maxRowPerPage);
+    }, [])
+
     return (
         <div className='Pagination'>
-            <RoundButton>
+            <RoundButton onClick={() => handelNavigation("prv")} >
                 <img src={arrow} className='arrow arrow_left' />
             </RoundButton >
-            <RoundButton>1</RoundButton>
-            <RoundButton>2</RoundButton>
-            <RoundButton>
+            {
+                getPageNumber().map(page => {
+                    return <RoundButton selected={page == curPage} onClick={() => handelNavigation(page)} key={page}>{page}</RoundButton>
+                })
+            }
+            <RoundButton onClick={() => handelNavigation("nxt")}>
                 <img src={arrow} className='arrow arrow_right' />
             </RoundButton >
         </div>
