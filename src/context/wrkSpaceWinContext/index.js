fb2bffec8e878f32b41b0baf08c522fe4d2d2d38
Suvel <suvelrathneswar08@gmail.com>
2022-06-16 19:22:23 +0530

    Added context for workSpace

---
+++ src/context/wrkSpaceWinContext/index.js
@@ -0,0 +1,32 @@
+import { createContext, useState } from 'react';
+
+export const WrkSpaceWinContext = createContext();
+
+
+const winTypes = {
+    "WT_FULL": "full",
+    "WT_MIN": "min",
+    "WT_NORMAL": "normal"
+}
+
+const Provider = ({ children }) => {
+    const [winType, setWinType] = useState(winTypes.WT_NORMAL);
+
+    const toggleWinType = (newWinType) => {
+        setWinType(newWinType)
+    }
+
+    return (
+        <WrkSpaceWinContext.Provider
+            value={{
+                windowType: winType,
+                toggleWindowType: toggleWinType,
+                availableWindowTypes: winTypes
+            }}
+        >
+            {children}
+        </WrkSpaceWinContext.Provider>
+    )
+}
+
+export default Provider;
