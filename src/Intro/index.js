14ec39457e38d3a97c7c54da0d39265ef31beb1f
Suvel <suvelrathneswar08@gmail.com>
2022-06-16 22:32:04 +0530

    Adding Intro page

--- src/Intro/index.js
+++ src/Intro/index.js
@@ -1,7 +1,12 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './style.css'
 import wel_emoji from '../asset/wel_emoji.png'
 import { WrkSpaceWinContext } from '../context/wrkSpaceWinContext'
+import ReactMarkdown from 'react-markdown'
+import aboutMD from '../content/about.md'
+import twitter_icon from '../asset/sm_twitter.svg'
+import fb_icon from '../asset/sm_fb.svg'
+import gmail_icon from '../asset/sm_gmail.svg'
 
 const ClickableText = ({ children, onClick }) => {
   return <span onClick={onClick} className='clickableText'>{children}</span>
@@ -9,11 +14,24 @@ const ClickableText = ({ children, onClick }) => {
 
 function Intro() {
 
+  const [applicationIntro, setApplicationIntro] = useState("")
   const { toggleWindowType, availableWindowTypes } = useContext(WrkSpaceWinContext);
 
   const handelAdjustingWorkSpaceWind = () => {
     toggleWindowType(availableWindowTypes.WT_MIN);
   }
+
+  useEffect(() => {
+    fetch(aboutMD).then((response) => response.text()).then((text) => {
+      debugger
+      setApplicationIntro(text)
+    })
+  }, [])
+
+  useEffect(() => {
+    console.log(applicationIntro)
+    debugger
+  }, [applicationIntro])
 
   return (
     <div className='Intro'>
@@ -28,6 +46,29 @@ function Intro() {
       </div>
       <div>
 
+      </div>
+      <div className='application_content'>
+        <div className="app_intro">
+          <ReactMarkdown children={applicationIntro} />
+        </div>
+        <div className="dev_info">
+          <div className="avatar">
+            <img src={wel_emoji} />
+          </div>
+          <div className="dev_detail">
+            <div className="dev_name">
+              Cody Fisher
+            </div>
+            <div className="dev_designation">
+              SE @ Barone LLC.
+            </div>
+            <div className="dev_contact">
+              <img src={twitter_icon} />
+              <img src={fb_icon} />
+              <img src={gmail_icon} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
