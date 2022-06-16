b972e90188310c159cfab0bdd5c05fa53a672089
Suvel <suvelrathneswar08@gmail.com>
2022-06-16 17:06:09 +0530

    Added section to navigate to show instruction of the applicaition

--- src/Intro/index.js
+++ src/Intro/index.js
@@ -7,7 +7,13 @@ function Intro() {
     <div className='Intro'>
       <div className='welcomeBanner'>
         <img src={wel_emoji} />
-        <h1>Welcome to the ADMIN PAGE</h1>
+        <span className='intro_content'>
+          <h1>Welcome to the ADMIN PAGE</h1>
+          <p><small>💡 Click here to read about the webpage</small></p>
+        </span>
+      </div>
+      <div>
+        
       </div>
     </div>
   )
