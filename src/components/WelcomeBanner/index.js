5333564f45d6138221cfa42a5d64abefc905133a
Suvel <suvelrathneswar08@gmail.com>
2022-06-18 10:54:34 +0530

    Added props to WelcomeBanner to toggle the clickable link

--- src/components/WelcomeBanner/index.js
+++ src/components/WelcomeBanner/index.js
@@ -3,14 +3,15 @@ import ClickableText from '../ClickableText'
 import wel_emoji from '../../asset/wel_emoji.png'
 
 const WelcomeBanner = ({
-  handelAdjustingWorkSpaceWind,
+  onClickableLinkClick,
+  hideClickableLink,
 }) => {
   return (
     <div className='welcomeBanner'>
       <img src={wel_emoji} />
       <span className='intro_content'>
         <h1>Welcome to the ADMIN PAGE</h1>
-        <ClickableText onClick={handelAdjustingWorkSpaceWind}>
+        <ClickableText onClick={onClickableLinkClick} disappear={hideClickableLink}>
           <p ><small>💡 Click here to read about the webpage</small></p>
         </ClickableText>
       </span>
