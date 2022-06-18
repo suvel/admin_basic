8e164cb604e7ffee15a1febf69bee2414c8b2fdb
Suvel <suvelrathneswar08@gmail.com>
2022-06-18 10:54:50 +0530

    Updated WelcomeBanner props

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import './style.css'
 import { WrkSpaceWinContext } from '../../context/wrkSpaceWinContext'
 import WelcomeBanner from '../WelcomeBanner'
@@ -9,19 +9,28 @@ import ShowWorkspaceButton from '../ShowWorkspaceButton'
 
 function Intro() {
 
-  const { toggleWindowType, availableWindowTypes } = useContext(WrkSpaceWinContext);
+  const { windowType, toggleWindowType, availableWindowTypes } = useContext(WrkSpaceWinContext);
+  const [hideClickableLink, setHideClickableLink] = useState(false);
 
   const handelAdjustingWorkSpaceWind = () => {
     toggleWindowType(availableWindowTypes.WT_MIN);
+    setHideClickableLink(true);
   }
 
   const handelShowingWorkspace = () => {
     toggleWindowType(availableWindowTypes.WT_NORMAL);
+    setHideClickableLink(false)
   }
+
+  useEffect(() => {
+    if (windowType !== availableWindowTypes.WT_MIN) {
+      setHideClickableLink(false);
+    }
+  }, [windowType])
 
   return (
     <div className='Intro'>
-      <WelcomeBanner handelAdjustingWorkSpaceWind={handelAdjustingWorkSpaceWind} />
+      <WelcomeBanner onClickableLinkClick={handelAdjustingWorkSpaceWind} hideClickableLink={hideClickableLink} />
       <div className='application_content'>
         <ShowWorkspaceButton
           showWorkspace={handelShowingWorkspace}
